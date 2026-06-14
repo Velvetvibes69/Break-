78b4e3d1407efab07d81169895d685d5984202ec
Velvetvibes69 <amayasunil11@gmail.com>
2026-06-14 19:18:51 +1200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+#Take user input
+a=input ("Enter a name: ")
+#Program to check break keyword
+for i in a: #itereate for loop
+    if i=="a": #condition#1
+        #display result
+        print("a is found")
+        break #break statement
+    else: #condition#2
+     print("a is not found")
